02ad4cee6cf8230e2e738ca9b484d858ae92a731
DamienHall <damien.z.hall@gmail.com>
2025-02-25 11:32:39 -0600

    Created Book Class

--- classes/Book.js
+++ classes/Book.js
@@ -1,6 +1,33 @@
 // import the Media class:
+const Media = require("./Media.js");
 
 // create your Book class:
+class Book extends Media {
+    // static methods
+    static highestRating(array) {
+        let highestRatedBook = array[0];
+
+        for (book of array) {
+            if (book.rating > highestRatedBook.rating) {
+                highestRatedBook = book;
+            }
+        }
+
+        return highestRatedBook;
+    } 
+
+    // Everthing else
+    constructor(title, year, genre, author, numPages, rating) {
+        super(title, year, genre);
+        this.author = author;
+        this.numPages = numPages;
+        this.rating = rating;
+    }
+
+    summary() {
+        return `Title: ${this.title}, Author: ${this.author}, Year: ${this.year}, Page Count: ${this.numPages}, Genre: ${this.genre}, Rating: ${this.rating}`;
+    }
+}
 
 // don't change below
 module.exports = Book;
